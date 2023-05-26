2531757dc6d0334cb72a78c51c4b870d67224ac7
Homatec <132045330+Cyril597@users.noreply.github.com>
2023-05-26 09:18:31 +0200

    Add files via upload

---
+++ Brief_4/inscription.js
@@ -0,0 +1,54 @@
+
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+
+    apiKey: "AIzaSyAw5n1MF8kJtHdQJfA8zfUtlP38tTAbMfk",
+  
+    authDomain: "mindgeek-292bd.firebaseapp.com",
+  
+    databaseURL: "https://mindgeek-292bd-default-rtdb.europe-west1.firebasedatabase.app",
+  
+    projectId: "mindgeek-292bd",
+  
+    storageBucket: "mindgeek-292bd.appspot.com",
+  
+    messagingSenderId: "1017837236790",
+  
+    appId: "1:1017837236790:web:452769bee3bcd445205e97",
+  
+    measurementId: "G-2Q3Z0V8ZGY"
+  
+  };
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ const auth = getAuth();
+
+ document.getElementById('formu').addEventListener('submit', function(event) {
+     event.preventDefault(); // Empêche le rechargement de la page après la soumission du formulaire
+
+     const email = document.getElementById('email').value;
+     const password = document.getElementById('mdp').value;
+
+ 
+     createUserWithEmailAndPassword(auth, email, password)
+       .then((userCredential) => {
+         // Signed in 
+         const user = userCredential.user;
+         console.log('Bien créé:', user);
+         // ...
+       })
+       .catch((error) => {
+         const errorCode = error.code;
+         const errorMessage = error.message;
+         console.error('Erreur lors de la création:', errorCode, errorMessage);
+         // ..
+       });
+
+    });
